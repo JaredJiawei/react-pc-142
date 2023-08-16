ee09857b42df76219aed8156a7269c4b61f77d53
unknown <jared.au0@gmail.com>
2023-08-16 22:07:22 +1000

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Login from './pages/Login'
+import Layout from './pages/Layout'
+import { Button, Space } from 'antd';
 
 function App() {
   return (
-    <div className="App">
-      app
-    </div>
+    <BrowserRouter>
+      <div className="App">
+      <Button type="primary">Primary Button</Button>
+        <Routes>
+        {/*创建路由patch和组件对应关系*/}
+          <Route path='/' element={<Layout></Layout>}></Route>
+          <Route path='/login' element={<Login></Login>}>
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
